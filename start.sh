07a00276d8de23f7b7ce942342be3b0e1ec1a694
MariuszMasewicz <mmasel@gmail.com>
2020-04-23 11:25:47 +0200

    oratop

--- start.sh
+++ start.sh
@@ -5,3 +5,6 @@ cd git
 git config --global user.name "MariuszMasewicz"
 git config --global user.email "mmasel@gmail.com"
 git clone git@github.com:MariuszMasewicz/dotfiles.git
+
+chmod 700 ~/oratop/oratop.RDBMS_12.1_LINUX_X64
+mv ~/oratop/oratop.RDBMS_12.1_LINUX_X64 ~/oratop/oratop
