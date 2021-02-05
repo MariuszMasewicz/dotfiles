ff94a49e1892ee5945b67a8cb6a225230eafae1d
MariuszMasewicz <mmasel@gmail.com>
2021-02-05 14:01:31 +0100

    nmon variable

--- install_tools.sh
+++ install_tools.sh
@@ -1,8 +1,15 @@
 yum install oracle-epel-release-el7
-yum install screen mc htop iftop
+dnf install oracle-epel-release-el8
+
+yum install screen mc htop iftop net-tools nmon etckeeper inxi upower tree wget
+dnf install tmux mc htop iftop net-tools nmon etckeeper
+
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install ./epel-release-latest-*.noarch.rpm
+
+dnf install bzip2 tar kernel-headers kernel-devel kernel-uek-devel elfutils-libelf-devel
 
 yum remove git
 yum install git222
